81849300b7f5a2e4ca10ec2e750e03c19e8b86a6
Dionid <don.diodos@gmail.com>
2017-11-30 10:47:13 +0300

    testing init name

--- src/mgos_arduino_liquidcrystal_i2c_c.c
+++ src/mgos_arduino_liquidcrystal_i2c_c.c
@@ -1,8 +1,8 @@
 #include <stdbool.h>
 
-bool mgos_arduino_liquidcrystal_i2c_init(void) {
-  return true;
-}
+// bool mgos_arduino_liquidcrystal_i2c_init(void) {
+//   return true;
+// }
 
 bool mgos_arduino_LiquidCrystal_I2C_init(void) {
   return true;
