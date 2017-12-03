9be6028ef1c8df7ea1acdcca66d7b5470469e6d7
Dionid <don.diodos@gmail.com>
2017-12-03 19:42:11 +0300

    Deleted size_t in return

--- src/mgos_arduino_liquidcrystal_i2c.cpp
+++ src/mgos_arduino_liquidcrystal_i2c.cpp
@@ -1,6 +1,6 @@
 #include "mgos_arduino_liquidcrystal_i2c.h"
 
-LiquidCrystal_I2C* mgos_liquidcrystal_i2c_create(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows) {
+LiquidCrystal_I2C* mgos_liquidcrystal_i2c_create(int lcd_Addr, int lcd_cols, int lcd_rows) {
   return new LiquidCrystal_I2C(lcd_Addr,lcd_cols,lcd_rows);
 }
 
@@ -26,22 +26,22 @@ void mgos_liquidcrystal_i2c_noBacklight(LiquidCrystal_I2C* lcd) {
   return lcd->noBacklight();
 }
 
-uint8_t mgos_liquidcrystal_i2c_getAddr(LiquidCrystal_I2C* lcd) {
+int mgos_liquidcrystal_i2c_getAddr(LiquidCrystal_I2C* lcd) {
   if (lcd == nullptr) return 0;
   return lcd->getAddr();
 }
 
-void mgos_liquidcrystal_i2c_setCursor(LiquidCrystal_I2C* lcd, uint8_t col, uint8_t row){
+void mgos_liquidcrystal_i2c_setCursor(LiquidCrystal_I2C* lcd, int col, int row){
   if (lcd == nullptr) return;
   return lcd->setCursor(col, row);
 }
 
-size_t mgos_liquidcrystal_i2c_print(LiquidCrystal_I2C* lcd, const char str[]) {
+int mgos_liquidcrystal_i2c_print(LiquidCrystal_I2C* lcd, const char str[]) {
   if (lcd == nullptr) return 0;
   return lcd->print(str);
 }
 
-size_t mgos_liquidcrystal_i2c_printChar(LiquidCrystal_I2C* lcd, char c) {
+int mgos_liquidcrystal_i2c_printChar(LiquidCrystal_I2C* lcd, char c) {
   if (lcd == nullptr) return 0;
   return lcd->print(c);
 }
@@ -131,22 +131,23 @@ void mgos_liquidcrystal_i2c_autoscroll(LiquidCrystal_I2C* lcd) {
   return lcd->autoscroll();
 }
 
+
 void mgos_liquidcrystal_i2c_noAutoscroll(LiquidCrystal_I2C* lcd) {
   if (lcd == nullptr) return;
   return lcd->noAutoscroll();
 }
 
-void mgos_liquidcrystal_i2c_createChar(LiquidCrystal_I2C* lcd, uint8_t location, uint8_t charmap[]) {
-  if (lcd == nullptr) return;
-  return lcd->createChar(location, charmap);
-}
+// void mgos_liquidcrystal_i2c_createChar(LiquidCrystal_I2C* lcd, int location, int *charmap) {
+//   if (lcd == nullptr) return;
+//   return lcd->createChar(location, charmap);
+// }
 
-size_t mgos_liquidcrystal_i2c_write(LiquidCrystal_I2C* lcd, uint8_t value) {
+int mgos_liquidcrystal_i2c_write(LiquidCrystal_I2C* lcd, int value) {
   if (lcd == nullptr) return 0;
   return lcd->write(value);
 }
 
-void mgos_liquidcrystal_i2c_command(LiquidCrystal_I2C* lcd, uint8_t value) {
+void mgos_liquidcrystal_i2c_command(LiquidCrystal_I2C* lcd, int value) {
   if (lcd == nullptr) return;
   return lcd->command(value);
 }
